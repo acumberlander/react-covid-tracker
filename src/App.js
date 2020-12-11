72692a28d9a469bcc75aacff81da16c1134ae02d
Austin Cumberlander <austincumberlander@gmail.com>
2020-12-11 17:44:03 -0600

    completed covid tracker code along

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Cards, Chart, CountryPicker } from './components';
 
 import styles from './App.module.css';
 import { fetchData } from './api';
+import CovidLogo from '../src/images/covid-tracker-logo.png';
 
 class App extends React.Component {
 	state = {
 		data: {},
+		country: '',
 	};
 
 	async componentDidMount() {
@@ -15,14 +17,21 @@ class App extends React.Component {
 		this.setState({ data: fetchedData });
 	}
 
+	handleCountryChange = async (country) => {
+		const fetchedData = await fetchData(country);
+
+		this.setState({ data: fetchedData, country: country });
+	};
+
 	render() {
-		const { data } = this.state;
+		const { data, country } = this.state;
 
 		return (
 			<div className={styles.container}>
+				<img className={styles.image} src={CovidLogo} alt="COVID-19" />
 				<Cards data={data} />
-				<CountryPicker />
-				<Chart />
+				<CountryPicker handleCountryChange={this.handleCountryChange} />
+				<Chart data={data} country={country} />
 			</div>
 		);
 	}
